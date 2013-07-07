12892c86c2d2401df6c0dcb02accda9d35cc7286
kyanro <gw.fnct+github-pub@gmail.com>
2013-07-07 23:35:25 +0900

    1st sample for my iis express

---
+++ AspTesting.Tests/AspFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using NUnit.Framework;
+
+namespace AspTesting.Tests
+{
+    public abstract class AspFixture
+    {
+        //private const string BASE_URI = "http://localhost:990/UnitTests/ASPTesting.Tests/";
+        private const string BASE_URI = "http://localhost:33211/";
+
+        public void RunAspTest(string uri)
+        {
+            var request = WebRequest.Create(BASE_URI + uri);
+
+            ICredentials requestCredentials = CredentialCache.DefaultCredentials;
+            request.Credentials = requestCredentials;
+
+            using (var response = request.GetResponse())
+            {
+                using (var stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string html = stream.ReadToEnd();
+
+                    Assert.IsFalse(html.Contains("Failed"));
+                }
+            }
+        }
+    }
+}
---
+++ AspTesting.Tests/TestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using AspTesting.Tests;
+
+namespace ASPTesting.Tests.UnitTests
+{
+    [TestFixture]
+    public class TestFixture : AspFixture
+    {
+        [Test]
+        public void Test()
+        {
+            RunAspTest("failed.html");
+        }
+    }
+}
